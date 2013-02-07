8e1c843536d1472a59af5e0bd55b225bce267c20
rparree <rparree@edc4it.com>
2013-02-07 13:11:28 +0100

    added salt to digested passwords

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
---run: "echo -n 'masterkey' | sha256sum"
+--run: "echo -n 'masterkey{username}' | sha256sum"
 insert into users (username,password,enabled)
   values
-  ('jennifer','48c5a1d217fe85082464d2ca1e90a16d15464fabe20f8610d79b63aa58797b9b',1),
-  ('john','48c5a1d217fe85082464d2ca1e90a16d15464fabe20f8610d79b63aa58797b9b',1);
+  ('jennifer','42e5b0e7c375f85225e7feca778fb79d89c06472737116685df6cb267bebb0f0',1),
+  ('john','ea1ec3fe37e28375adcd9a708d0a6172ce618ba8ba69f9270b349e2e73f662c6',1);
 
 insert into authorities(username,authority)
   values
